4860786e3e3e7ea6faf74b7b6a0b914147ded7ba
tasnimesahli <tasnimsahli97@gmail.com>
2025-12-31 13:53:15 +0100

    done

--- mpd.sql
+++ mpd.sql
@@ -43,3 +43,27 @@ mot_de_passe varchar(600) not null,
 id_chef int,
 foreign key(id_chef) references personnel(id_personnel) 
 );
+insert into rayon (nom) values
+('Informatique'),
+('Littérature'),
+('Science'),
+('Histoire');
+insert into ouvrage (titre, date_publication, id_rayon) values
+('Python pour débutants', '2021-03-10', 1),
+('Les Misérables', '1862-01-01', 2),
+('Physique moderne', '2015-07-15', 3),
+('Histoire du Maroc', '2018-05-20', 4);
+insert into auteur (nom, prenom) values
+('Dupont', 'Jean'),
+('Hugo', 'Victor'),
+('Einstein', 'Albert'),
+('El Mansouri', 'Ahmed');
+insert into lecteur (nom, prenom, email, cin, tel) values
+('Ali', 'Karim', 'ali.karim@mail.com', 'AB123456', '0612345678'),
+('Sara', 'Mouad', 'sara.mouad@mail.com', 'CD789012', '0698765432');
+insert into emprunt (date_emprunt, date_retour_prevue, date_retour_reel, id_ouvrage, id_lecteur) values
+('2025-12-01', '2025-12-15','2025-12-17', 1, 1),
+('2025-12-05', '2025-12-20', '2025-12-304', 2, 2);
+insert into personnel (nom, prenom, email, mot_de_passe, id_chef) values
+('Benzouina', 'Rachid', 'rachid.benzouina@mail.com', 'pass123', NULL),
+('Haddad', 'Laila', 'laila.haddad@mail.com', 'pass456', 1);

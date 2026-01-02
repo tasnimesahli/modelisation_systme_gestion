844447fa5d59ebf0ef9b0aac857dd45d3fe5522d
tasnimesahli <tasnimsahli97@gmail.com>
2026-01-02 09:59:37 +0100

    Add auteur_ouvrage table and related insert statement

--- mpd.sql
+++ mpd.sql
@@ -24,6 +24,13 @@ email varchar(500) not null unique,
 cin varchar(100) not null unique ,
 tel varchar(15) not null unique 
 );
+create table auteur_ouvrage (
+    id_ouvrage INT,
+    id_auteur INT,
+    PRIMARY KEY (id_ouvrage, id_auteur),
+    FOREIGN KEY (id_ouvrage) REFERENCES ouvrage(id_ouvrage),
+    FOREIGN KEY (id_auteur) REFERENCES auteur(id_auteur)
+);
 create table emprunt(
 id_emprunt int not null auto_increment primary key,
 date_emprunt date not null,
@@ -61,6 +68,7 @@ insert into auteur (nom, prenom) values
 insert into lecteur (nom, prenom, email, cin, tel) values
 ('Ali', 'Karim', 'ali.karim@mail.com', 'AB123456', '0612345678'),
 ('Sara', 'Mouad', 'sara.mouad@mail.com', 'CD789012', '0698765432');
+insert into auteur_ouvrage value (1,1), (2,2);
 insert into emprunt (date_emprunt, date_retour_prevue, date_retour_reel, id_ouvrage, id_lecteur) values
 ('2025-12-01', '2025-12-15','2025-12-17', 1, 1),
 ('2025-12-05', '2025-12-20', '2025-12-304', 2, 2);
